755ea7cd4b25ac077648f3330aca0fa10302c807
0815Sailsman <sailsman@tutanota.com>
2024-05-12 19:46:48 +0200

    switch process library

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,31 @@
 use std::process::exit;
-use tasklist;
-use tasklist::Process;
+use proc_mem::Process;
 
 fn main() {
-    let mut tl = unsafe { tasklist::Tasklist::new() };
-    let dark_souls_remastered: Process = tl.find(|it| it.pname.to_lowercase().eq("darksoulsremastered.exe")).unwrap_or_else(|| {
-        eprintln!("No process for Dark Souls:Remastered has been found");
+    let proc_name = "DarkSoulsRemastered.exe";
+
+    let dark_souls_remastered = Process::with_name(proc_name).unwrap_or_else(|e| {
+        eprintln!("Error opening process: {:?}", e);
         exit(1);
     });
-    println!("{} {} {}", dark_souls_remastered.get_pid(), dark_souls_remastered.get_pname(), dark_souls_remastered.get_user());
+
+    println!("Found process: {} {}", dark_souls_remastered.process_id, dark_souls_remastered.process_name);
+    // resolve_pointers(dark_souls_remastered);
 }
+
+/*fn resolve_pointers(process: Process) {
+    let baseAddress = process.MainModule.BaseAddress.ToInt64();
+    let bytes = process.ReadProcessMemory(baseAddress, process.MainModule.ModuleMemorySize).Unwrap();
+    let is64Bit = process.Is64Bit().Unwrap();
+}*/
+
+/*
+        NodeType = NodeType.RelativeScan,
+        Name = "GameMan",
+        Pattern = "48 8b 05 ? ? ? ? c6 40 18 00",
+        AddressOffset = 3,
+        InstructionSize = 7,
+    };
+.AddPointer(//pointer name_gameMan, // offsets 0)
+;
+ */
