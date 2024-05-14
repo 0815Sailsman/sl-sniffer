efbb11505ea2050a0fe60f51871b1e3846aa848a
0815Sailsman <sailsman@tutanota.com>
2024-05-14 23:30:45 +0200

    move module and base address down

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ struct Pointer {
     offsets: Vec<usize>,
 }
 
+// todo maybe add fields here for module and base address, so they dont have to be recomputed all the time?
 struct DarkSoulsRemastered {
     process: Process,
     player_position: Option<Pointer>,
@@ -35,21 +36,13 @@ fn main() {
 }
 
 fn resolve_pointers(dark_souls_remastered: &mut DarkSoulsRemastered) {
-    let base_address = dark_souls_remastered.process.process_base_address;
-    println!("Base address: {:#x?}", base_address);
-    let module = dark_souls_remastered.process.module("DarkSoulsRemastered.exe").unwrap_or_else(|e| {
-        eprintln!("Error opening module: {:?}", e);
-        exit(2);
-    });
-    println!("Module base size: {:?}", module.base_size());
-
     dark_souls_remastered.player_position = Some(Pointer {
         base_address: init_address_from_node(PointerNode {
             name: String::from("player_pos"),
             pattern: String::from("48 8b 0d ? ? ? ? 0f 28 f1 48 85 c9 74 ? 48 89 7c"),
             address_offset: 3,
             instruction_size: 7,
-        }, &dark_souls_remastered.process, &module, base_address),
+        }, &dark_souls_remastered),
         offsets: vec![0, 0x68, 0x68, 0x28],
     });
 }
@@ -62,7 +55,7 @@ fn read_player_position(position_pointer: &Pointer, process: &Process) -> Vec<f3
     ];
 }
 
-fn init_address_from_node(pointer_node: PointerNode, process: &Process, module: &Module, base_address: usize) -> usize {
+fn init_address_from_node(pointer_node: PointerNode, dark_souls_remastered: &DarkSoulsRemastered) -> usize {
     let signature = Signature {
         name: pointer_node.name,
         pattern: pointer_node.pattern,
@@ -72,13 +65,22 @@ fn init_address_from_node(pointer_node: PointerNode, process: &Process, module: 
         rip_relative: false,
         rip_offset: 0,
     };
+
+    let module = dark_souls_remastered.process.module("DarkSoulsRemastered.exe").unwrap_or_else(|e| {
+        eprintln!("Error opening module: {:?}", e);
+        exit(2);
+    });
+
     let initial_search = module.find_signature(&signature).unwrap_or_else(|e| {
         eprintln!("Error obtaining player_pos_address: {:?}", e);
         exit(3);
     });
     println!("initial search result: {:#x?}", initial_search);
 
-    let address_at_initial_search = process.read_mem::<i32>(base_address + initial_search + pointer_node.address_offset).unwrap();
+    let base_address = dark_souls_remastered.process.process_base_address;
+    let address_at_initial_search = dark_souls_remastered.process.read_mem::<i32>(
+        base_address + initial_search + pointer_node.address_offset
+    ).unwrap();
     println!("address at initial search: {:#x?}", address_at_initial_search);
 
     let target_address = base_address + initial_search + address_at_initial_search as usize + pointer_node.instruction_size;
