70f2e0ac9a3061dfae2a62e6479c6d96e1b4ae5d
0815Sailsman <sailsman@tutanota.com>
2024-05-14 23:58:03 +0200

    late night refactoring module into game-struct. I NEED TO SLEEP NOW!

--- src/main.rs
+++ src/main.rs
@@ -8,31 +8,55 @@ struct PointerNode {
     instruction_size: usize,
 }
 
+// note maybe move process from dark souls remastered struct into pointer struct
+#[derive(Clone)]
 struct Pointer {
     base_address: usize,
     offsets: Vec<usize>,
 }
 
-// todo maybe add fields here for module and base address, so they dont have to be recomputed all the time?
 struct DarkSoulsRemastered {
     process: Process,
+    module: Module,
     player_position: Option<Pointer>,
 }
 
+// note generally: are classes in rust a thing? do I want them?
 fn main() {
+    let process = Process::with_name("DarkSoulsRemastered.exe").unwrap_or_else(|e| {
+        eprintln!("Error opening process: {:?}", e);
+        exit(1);
+    });
+    println!("Found process: {} {}", process.process_id, process.process_name);
+
+    let module = process.module("DarkSoulsRemastered.exe").unwrap_or_else(|e| {
+        eprintln!("Error opening module: {:?}", e);
+        exit(2);
+    });
+
+    // note can I create this struct in a nicer way? temporarily initializing the 2 beforehand seems uncool
     let mut dark_souls_remastered: DarkSoulsRemastered = DarkSoulsRemastered {
-        process: Process::with_name("DarkSoulsRemastered.exe").unwrap_or_else(|e| {
-            eprintln!("Error opening process: {:?}", e);
-            exit(1);
-        }),
+        process,
+        module,
         player_position: None,
     };
-    println!("Found process: {} {}", dark_souls_remastered.process.process_id, dark_souls_remastered.process.process_name);
 
+    // I like this
     resolve_pointers(&mut dark_souls_remastered);
 
-    let coords = read_player_position(&dark_souls_remastered.player_position.unwrap(), &dark_souls_remastered.process);
+    let coords = read_player_position(&dark_souls_remastered);
     println!("Player pos: (x:{:#?}, y:{:#?}, z:{:#?})", coords[0], coords[1], coords[2]);
+}
+
+fn read_player_position(dark_souls_remastered: &DarkSoulsRemastered) -> Vec<f32> {
+    // note why do I need to clone here? How do I make this better?
+    let player_position_copy = dark_souls_remastered.player_position.clone()
+        .expect("Tried to read player position without initializing the pointer");
+    return vec![
+        read_float(&player_position_copy, 0x10, &dark_souls_remastered.process),
+        read_float(&player_position_copy, 0x14, &dark_souls_remastered.process),
+        read_float(&player_position_copy, 0x18, &dark_souls_remastered.process),
+    ];
 }
 
 fn resolve_pointers(dark_souls_remastered: &mut DarkSoulsRemastered) {
@@ -47,14 +71,6 @@ fn resolve_pointers(dark_souls_remastered: &mut DarkSoulsRemastered) {
     });
 }
 
-fn read_player_position(position_pointer: &Pointer, process: &Process) -> Vec<f32> {
-    return vec![
-        read_float(&position_pointer, 0x10, &process),
-        read_float(&position_pointer, 0x14, &process),
-        read_float(&position_pointer, 0x18, &process),
-    ];
-}
-
 fn init_address_from_node(pointer_node: PointerNode, dark_souls_remastered: &DarkSoulsRemastered) -> usize {
     let signature = Signature {
         name: pointer_node.name,
@@ -66,24 +82,18 @@ fn init_address_from_node(pointer_node: PointerNode, dark_souls_remastered: &Dar
         rip_offset: 0,
     };
 
-    let module = dark_souls_remastered.process.module("DarkSoulsRemastered.exe").unwrap_or_else(|e| {
-        eprintln!("Error opening module: {:?}", e);
-        exit(2);
-    });
-
-    let initial_search = module.find_signature(&signature).unwrap_or_else(|e| {
+    let initial_search = dark_souls_remastered.module.find_signature(&signature).unwrap_or_else(|e| {
         eprintln!("Error obtaining player_pos_address: {:?}", e);
         exit(3);
     });
     println!("initial search result: {:#x?}", initial_search);
 
-    let base_address = dark_souls_remastered.process.process_base_address;
     let address_at_initial_search = dark_souls_remastered.process.read_mem::<i32>(
-        base_address + initial_search + pointer_node.address_offset
+        dark_souls_remastered.process.process_base_address + initial_search + pointer_node.address_offset
     ).unwrap();
     println!("address at initial search: {:#x?}", address_at_initial_search);
 
-    let target_address = base_address + initial_search + address_at_initial_search as usize + pointer_node.instruction_size;
+    let target_address = dark_souls_remastered.process.process_base_address + initial_search + address_at_initial_search as usize + pointer_node.instruction_size;
     println!("Target address: {:#x?}", target_address);
 
     return target_address;
