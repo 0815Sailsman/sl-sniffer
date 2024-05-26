628c5d824fc42a6a6db02523591a8634531eb7d8
Sailsman <sailsman@tutanota.com>
2024-05-26 21:06:10 +0200

    extract init method in main

--- src/main.rs
+++ src/main.rs
@@ -13,26 +13,7 @@ mod bonfire;
 mod remastered;
 
 fn main() {
-    let process = Process::with_name("DarkSoulsRemastered.exe").unwrap_or_else(|e| {
-        eprintln!("Error opening process: {:?}", e);
-        exit(1);
-    });
-    println!("Found process: {} {}", process.process_id, process.process_name);
-
-    let module = process.module("DarkSoulsRemastered.exe").unwrap_or_else(|e| {
-        eprintln!("Error opening module: {:?}", e);
-        exit(2);
-    });
-
-    // note can I create this struct in a nicer way? temporarily initializing the 2 beforehand seems uncool
-    let mut dark_souls_remastered: DarkSoulsRemastered = DarkSoulsRemastered {
-        process,
-        module,
-        player_game_data: None,
-        player_position: None,
-        player_ins: None,
-        bonfire_db: None
-    };
+    let mut dark_souls_remastered = init_remastered();
 
     // I like this even more
     dark_souls_remastered.resolve_pointers();
@@ -54,4 +35,28 @@ fn main() {
 
     let bonfire_state_firelink_shrine = dark_souls_remastered.get_bonfire_state(Bonfire::FirelinkShrine);
     println!("Bonfire firelink: {:#?}", bonfire_state_firelink_shrine);
+}
+
+fn init_remastered() -> DarkSoulsRemastered {
+    let process = Process::with_name("DarkSoulsRemastered.exe").unwrap_or_else(|e| {
+        eprintln!("Error opening process: {:?}", e);
+        exit(1);
+    });
+    println!("Found process: {} {}", process.process_id, process.process_name);
+
+    let module = process.module("DarkSoulsRemastered.exe").unwrap_or_else(|e| {
+        eprintln!("Error opening module: {:?}", e);
+        exit(2);
+    });
+
+    // note can I create this struct in a nicer way? temporarily initializing the 2 beforehand seems uncool
+    let mut dark_souls_remastered: DarkSoulsRemastered = DarkSoulsRemastered {
+        process,
+        module,
+        player_game_data: None,
+        player_position: None,
+        player_ins: None,
+        bonfire_db: None
+    };
+    dark_souls_remastered
 }
